dfd1c8016f6ecfd4d177c3b6cf36e4ba8092dda8
Rohit Kadam <upskillrohit@gmail.com>
2026-05-10 10:17:41 +0530

    initial commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import SafarSaathiLogin from './Pages/Login'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <SafarSaathiLogin />
+    </>
+  )
+}
+
+export default App
